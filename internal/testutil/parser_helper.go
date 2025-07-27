b8653d4392e6b7e0cc4297eb0c8bc9ca219360df
IgorBayerl <dev.igorbayerl@gmail.com>
2025-07-27 16:00:45 +0200

    add gcov parser with correct cpp function name extracting

--- internal/testutil/parser_helper.go
+++ internal/testutil/parser_helper.go
@@ -6,9 +6,9 @@ import (
 
 	"github.com/IgorBayerl/AdlerCov/internal/filtering"
 	"github.com/IgorBayerl/AdlerCov/internal/language"
+	"github.com/IgorBayerl/AdlerCov/internal/language/cpp"
 	"github.com/IgorBayerl/AdlerCov/internal/language/csharp"
 	"github.com/IgorBayerl/AdlerCov/internal/language/defaultformatter"
-	"github.com/IgorBayerl/AdlerCov/internal/language/gcc"
 	"github.com/IgorBayerl/AdlerCov/internal/language/golang"
 	"github.com/IgorBayerl/AdlerCov/internal/settings"
 )
@@ -40,7 +40,7 @@ func NewTestConfig(sourceDirs []string) *MockParserConfig {
 		defaultformatter.NewDefaultProcessor(),
 		golang.NewGoProcessor(),
 		csharp.NewCSharpProcessor(),
-		gcc.NewGccProcessor(),
+		cpp.NewCppProcessor(),
 	)
 
 	return &MockParserConfig{
