a86b0cdd10631e9b2523b5a179c78f9b6a345345
IgorBayerl <dev.igorbayerl@gmail.com>
2025-08-26 21:45:49 +0200

    removing old angular project

--- internal/assets/angular_frontend_spa/src/app/components/riskhotspots/data/riskhotspots-settings.class.ts
+++
@@ -1,8 +0,0 @@
-export class RiskHotspotsSettings {
-  assembly: string = "";
-  numberOfRiskHotspots: number = 10;
-  filter: string = "";
-
-  sortBy: string = "";
-  sortOrder: string = "asc";
-}
